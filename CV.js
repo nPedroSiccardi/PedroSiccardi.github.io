00345e17fca205c67525308be2116c0033d99807
nPedroSiccardi <124076461+nPedroSiccardi@users.noreply.github.com>
2023-01-31 14:12:37 -0300

    Add files via upload

---
+++ CV.js
@@ -0,0 +1,38 @@
+let mybutton = document.getElementsById("myBtn");
+let button1 = document.getElementById("p1")
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+function boton1() {
+  var text = document.getElementById("p1");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+function boton2() {
+  var text = document.getElementById("p2");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+function boton3() {
+  var text = document.getElementById("p3");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+function boton4() {
+  var text = document.getElementById("p4");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
